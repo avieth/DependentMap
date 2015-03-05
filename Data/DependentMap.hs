48dc07ab7ce5e85b6d361c7358daa8962fde8690
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-03-04 21:59:35 -0500

    Add notes about folding, Monoid instance

--- Data/DependentMap.hs
+++ Data/DependentMap.hs
@@ -14,6 +14,7 @@ module Data.DependentMap (
   , dLookup
   , dDelete
   , dFoldWithKey
+  , dSize
 
   , DMapFunction
 
@@ -22,6 +23,7 @@ module Data.DependentMap (
   ) where
 
 import Data.Typeable
+import Data.Monoid
 
 heteroEq :: forall v a b . (Eq (v a), Typeable a, Typeable b) => v a -> v b -> Bool
 heteroEq x y = case eqT :: Maybe (a :~: b) of
@@ -73,10 +75,11 @@ dInsert
   :: ( Eq (k a)
      , Ord (k a)
      , Typeable a
-     , Typeable (DMapFunction t a)
+     , Typeable b
+     , b ~ DMapFunction t a
      )
   => k a
-  -> v (DMapFunction t a)
+  -> v b
   -> DMap t k v
   -> DMap t k v
 dInsert key val dmap = case dmap of
@@ -117,14 +120,66 @@ dDelete key dmap = case dmap of
   DMapNil -> DMapNil
   DMapCons key' _ rest -> if heteroEq key key' then rest else dDelete key dmap
 
+-- How could a fold possibly work?
+-- The provided function has to switch its behaviour based on the type
+-- parameter!
+-- This attempt is not very useful; all we can use is functions parametrically
+-- polymorphic in the parameter!
 dFoldWithKey
   :: forall t k v r .
      (
      )
-  => (forall k v a . k a -> v (DMapFunction t a) -> r -> r)
+  => (forall a . k a -> v (DMapFunction t a) -> r -> r)
   -> r
   -> DMap t k v
   -> r
 dFoldWithKey f b dmap = case dmap of
   DMapNil -> b
   DMapCons key val rest -> dFoldWithKey f (f key val b) rest
+
+-- The problem: you just don't know what parameters could be used in the map!
+-- Yet still the function you give must work on ANY parameter! We have this
+-- problem even for the earlier version, without DMapFunction. If your k and
+-- v types give enough information, then that's great. If not, then you're
+-- out of luck.
+dFoldWithKey'
+  :: forall t k v r .
+     (
+     )
+  => (forall a . k a -> v (DMapFunction t a) -> r -> r)
+  -> r
+  -> DMap t k v
+  -> r
+dFoldWithKey' f b dmap = case dmap of
+  DMapNil -> b
+  DMapCons key val rest -> dFoldWithKey f (f key val b) rest
+
+-- Another problem: we demand Typeable (DMapFunction t a) FOR EVERY a!!!!
+-- Damn...
+-- Hm, can't we just take it at insert time, then recover it from pattern
+-- matching on DMapCons?
+
+dUnion
+  :: forall t k v .
+     (
+     )
+  => DMap t k v
+  -> DMap t k v
+  -> DMap t k v
+dUnion dmapLeft dmapRight = case dmapRight of
+  DMapNil -> dmapLeft
+  DMapCons key val rest -> dInsert key val (dUnion dmapLeft rest)
+-- Interesting: the above works, but the below does not!
+--dUnion dmapLeft dmapRight = dFoldWithKey union dmapLeft dmapRight
+
+dSize
+  :: forall t k v .
+     (
+     )
+  => DMap t k v
+  -> Int
+dSize dmap = dFoldWithKey (\_ _ i -> i + 1) 0 dmap
+
+instance Monoid (DMap t k v) where
+  mempty = dEmpty
+  mappend = dUnion
